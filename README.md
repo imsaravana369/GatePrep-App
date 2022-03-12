f6cbbe20ec151e39aa504d4433e53eac93091853
imsaravana369 <imsaravana369@gmail.com>
2022-03-12 09:09:27 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# gate_prep_app
+
+An app for students preparing for Gate Exam.
+
---
+++ android/app/src/main/kotlin/com/saravana/gate_prep_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.saravana.gate_prep_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
